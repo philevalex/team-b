70658f39c3802e4065e08bb216a869360533a385
63110 <63110for@list.ru>
2020-09-18 20:52:23 +0300

    lla

--- todo.js
+++ todo.js
@@ -1,26 +1,31 @@
 "use strict"
 const todos = [1,2,3,4];
 
-const Id=0;
+const id=0;
 
 function getId() {
-    Id++;
-    return Id;
+    id++;
+    return id;
 }
-function addtodo (NewTodo){
+function addtodo (newTodo){
     todos.push({id: getId(), title: NewTodo, isComplete: false });
 }
-function deletetodo (Todo)
+function deletetodo (todoid)
 {
+    const index = todos.findIndex((todo) => todo.id === todoId);
+    if (index === -1) {
+        throw new Error('Id not found');
+    }
+    todos.splice(index, 1)
 
 }
 function statusId()
 {
-    todos.push(NewTodo)
+    todos.push(newTodo)
 
 }
 function  ()
 {
-    todos.push(NewTodo)
+    todos.push(newTodo)
 
 }
