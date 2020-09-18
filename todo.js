2cf061b64bf556169ad1bd15c00bf56be41e9836
63110 <63110for@list.ru>
2020-09-18 16:07:37 +0300

    addjs

---
+++ todo.js
@@ -0,0 +1,26 @@
+"use strict"
+const todos = [1,2,3,4];
+
+const Id=0;
+
+function getId() {
+    Id++;
+    return Id;
+}
+function addtodo (NewTodo){
+    todos.push({id: getId(), title: NewTodo, isComplete: false });
+}
+function deletetodo (Todo)
+{
+
+}
+function statusId()
+{
+    todos.push(NewTodo)
+
+}
+function  ()
+{
+    todos.push(NewTodo)
+
+}
